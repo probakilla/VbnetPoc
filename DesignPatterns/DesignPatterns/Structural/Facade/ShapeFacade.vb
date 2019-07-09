d297c54e133de8261972fbb8e283f45ba649602e
Julien <j.pilleux@gmail.com>
2019-07-09 10:06:38 +0200

    Implementation of a facade

---
+++ DesignPatterns/DesignPatterns/Structural/Facade/ShapeFacade.vb
@@ -0,0 +1,21 @@
+﻿Imports DesignPatterns.Shapes
+
+Namespace Facade
+    Public Class ShapeFacade
+        Private Circle As IShape
+        Private Rectangle As IShape
+
+        Public Sub New()
+            Circle = New Circle(2)
+            Rectangle = New Rectangle(2, 2)
+        End Sub
+
+        Public Function DrawCircle() As String
+            Return Circle.Draw
+        End Function
+
+        Public Function DrawRectangle() As String
+            Return Rectangle.Draw
+        End Function
+    End Class
+End Namespace
---
+++ PocUnitTesting/PatternTests/FacadeTest.vb
@@ -0,0 +1,21 @@
+﻿Imports DesignPatterns.Facade
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Namespace PocUnitTesting
+    <TestClass>
+    Public Class FacadeTest
+        <TestMethod>
+        Public Sub FacadeRectangleDrawTest()
+            Dim facade = New ShapeFacade
+            Dim rectangleDrawing = facade.DrawRectangle
+            Assert.AreEqual("[Rectangle] Width : 2, Height : 2", rectangleDrawing)
+        End Sub
+
+        <TestMethod>
+        Public Sub FacadeCircleDrawTest()
+            Dim facade = New ShapeFacade
+            Dim circleDrawing = facade.DrawCircle
+            Assert.AreEqual("[Circle] Radius : 2", circleDrawing)
+        End Sub
+    End Class
+End Namespace
